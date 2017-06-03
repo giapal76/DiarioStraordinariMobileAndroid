4eb95eaf4ec727112ec8541d78a14a99ad08c342
Gianni <giapal76>
2017-06-03 17:59:28 +0200

    /* */

--- app/src/main/java/com/example/andrea/diariostraordinari/API/APIservice.java
+++ app/src/main/java/com/example/andrea/diariostraordinari/API/APIservice.java
@@ -1,6 +1,9 @@
 package com.example.andrea.diariostraordinari.API;
 
 import com.example.andrea.diariostraordinari.result.result_accesso;
+import com.example.andrea.diariostraordinari.result.result_insert;
+import com.example.andrea.diariostraordinari.result.result_listaUtenti;
+
 
 import retrofit2.Call;
 import retrofit2.http.Field;
@@ -12,4 +15,10 @@ public interface APIservice {
     @FormUrlEncoded
     @POST("accesso")
     Call<result_accesso> accessoUtente(@Field("idattore")String idattore, @Field("password")String password);
+
+    @FormUrlEncoded
+    @POST("insert")
+    Call<result_insert> insertUtente(@Field("idattore")String idattore, @Field("tipo")String tipo, @Field("nome")String nome,
+                                     @Field("cognome")String cognome, @Field("password")String password);
+
 }
--- app/src/main/java/com/example/andrea/diariostraordinari/Activity/Nuovo_operaioActivity.java
+++ app/src/main/java/com/example/andrea/diariostraordinari/Activity/Nuovo_operaioActivity.java
@@ -13,12 +13,25 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.andrea.diariostraordinari.Adapter.Attore;
 import com.example.andrea.diariostraordinari.Adapter.AttoriListAdapter;
 import com.example.andrea.diariostraordinari.R;
+
+import com.example.andrea.diariostraordinari.result.result_accesso;
+import com.example.andrea.diariostraordinari.result.result_insert;
+import com.example.andrea.diariostraordinari.API.APIservice;
+import com.example.andrea.diariostraordinari.API.APIurl;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 import java.util.ArrayList;
@@ -68,119 +81,61 @@ public class Nuovo_operaioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nuovo_operaio);
 
-
-        spinnerAttori2 = (AppCompatSpinner) findViewById(R.id.dbaSelezioneAttoriSpinner);
-
-        //Si crea un Array di stringhe per visualizzarle negli spinner
         ArrayAdapter<CharSequence> adapterAttori = ArrayAdapter.createFromResource(this, R.array.attori_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
         adapterAttori.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Applico l'adapter allo spinner
         spinnerAttori2.setAdapter(adapterAttori);
 
         insertId = (EditText) findViewById(R.id.newIdEditText);
+        spinnerAttori2 = (AppCompatSpinner) findViewById(R.id.dbaSelezioneAttoriSpinner) ;
         insertName = (EditText) findViewById(R.id.newNameEditText);
         insertSurname = (EditText) findViewById(R.id.newSurnameEditText);
         insertPass = (EditText) findViewById(R.id.newPassEditText);
 
         final Button insertButton = (Button) findViewById(R.id.new_Insert);
 
-        /*** DA IMPLEMENTARE ***/
-        /*** RIF. 1 ***/
-        //Aggiungo un ClickListener per inserire il nuovo utente nel DB
-
         insertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                insert();
+            }
 
-                inserisciUtenteInDB(myView);
+            private void insert() {
+                String idattore = insertId.getText().toString();
+                String tipo = spinnerAttori2.getSelectedItem().toString();
+                String nome = insertName.getText().toString();
+                String cognome = insertSurname.getText().toString();
+                String password = insertPass.getText().toString();
 
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(APIurl.BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+
+                APIservice service = retrofit.create(APIservice.class);  //accesso a tutti i servizi che ho fatto
+
+                Call<result_insert> call = service.insertUtente(idattore, tipo, nome, cognome, password);
+                call.enqueue(new Callback<result_insert>() {
+                    @Override
+                    public void onResponse(Call<result_insert> call, Response<result_insert> response) {
+                        Toast.makeText(Nuovo_operaioActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                        reset();
+                    }
+
+                    @Override
+                    public void onFailure(Call<result_insert> call, Throwable t) {
+                        Toast.makeText(Nuovo_operaioActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
-        myView = findViewById(R.id.newOpContainerLayout);
-
-
-
     }
 
-    /*** DA IMPLEMENTARE ***/
-    /*** RIF. 2 ***/
-   //Tasto indietro
-    @Override
-    public void onBackPressed() {
-        exitByBackKey();
-    }
-
-    protected void exitByBackKey() {
-
-        //Qui creo un'AlertDialog per avvisare l'utente che si sta tornando a Nuovo_operaioActivity
-        AlertDialog alertbox = new AlertDialog.Builder(this)
-                .setMessage(getString(R.string.back_DBA_request))
-                .setPositiveButton("Sì", new DialogInterface.OnClickListener() {
-
-                    // do something when the button is clicked
-                    public void onClick(DialogInterface arg0, int arg1) {
-
-                        Intent i = new Intent(Nuovo_operaioActivity.this, DBAActivity.class);
-                        startActivity(i);
-
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-
-                    // do something when the button is clicked
-                    public void onClick(DialogInterface arg0, int arg1) {
-                    }
-                })
-                .show();
-
-    }
-
-
-    /*** DA IMPLEMENTARE ***/
-    /*** VEDI RIF.1 ***/
-
-    // Oggetto FirebaseDatabase per comunicare con il DB
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference mDatabase = database.getReference();
-
-    private void inserisciUtenteInDB(View v){
-        String tipoAttore = "";
-        String userId = "";
-        String nome = "";
-        String cognome = "";
-        String password = "";
-        tipoAttore = spinnerAttori2.getSelectedItem().toString();
-        userId = insertId.getText().toString();
-        nome = insertName.getText().toString();
-        cognome = insertSurname.getText().toString();
-        password = insertPass.getText().toString();
-
-        writeNewUser(tipoAttore, userId, nome, cognome, password);
-
-        //Per il momento segnalo a video che ci stiamo lavorando
-        Snackbar.make(v, getString(R.string.salvataggio_riuscito), Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
-
-        //Cancello le stringhe dopo l'inserimento
-        insertId.setText(" ");
-        insertName.setText(" ");
-        insertSurname.setText(" ");
-        insertPass.setText(" ");
-
-    }
-
-    private void writeNewUser(String tipoAttore, String userId, String nome, String cognome, String password) {
-
-        String table = "AttoriTestApp";
-
-        Attore user = new Attore(tipoAttore, nome, cognome, password);
-
-        mDatabase.child(table).child(tipoAttore).child(userId).setValue(user);
-
-    }
-
-
+private void reset (){
+    insertId.setText(" ");
+    insertName.setText(" ");
+    insertSurname.setText(" ");
+    insertPass.setText(" ");
+}
 
 }
