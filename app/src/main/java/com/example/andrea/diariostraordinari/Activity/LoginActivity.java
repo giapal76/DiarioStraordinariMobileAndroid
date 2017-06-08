41a27f2b6bc5defaf822bd69c54840372ae6810b
Gianni <giapal76>
2017-06-08 18:38:21 +0200

    8.6

--- app/src/main/java/com/example/andrea/diariostraordinari/Activity/LoginActivity.java
+++ app/src/main/java/com/example/andrea/diariostraordinari/Activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.andrea.diariostraordinari.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,8 @@ import com.example.andrea.diariostraordinari.result.result_accesso;
 import com.example.andrea.diariostraordinari.API.APIservice;
 import com.example.andrea.diariostraordinari.API.APIurl;
 import com.example.andrea.diariostraordinari.Adapter.Attore;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText editTextEmail, editTextPassword;
     Button buttonAccesso;
+    private View loginFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         editTextEmail = (EditText) findViewById(R.id.matricola);
         editTextPassword = (EditText) findViewById(R.id.password);
         buttonAccesso = (Button) findViewById(R.id.sign_in_button);
+        loginFormView = findViewById(R.id.login_form);
 
         buttonAccesso.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,16 +59,58 @@ public class LoginActivity extends AppCompatActivity {
                 call.enqueue(new Callback<result_accesso>() {
                     @Override
                     public void onResponse(Call<result_accesso> call, Response<result_accesso> response) {
-                        Toast.makeText(LoginActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                    apriNuovaSchermata(response.body().getMessage());
                     }
 
                     @Override
                     public void onFailure(Call<result_accesso> call, Throwable t) {
-                        Toast.makeText(LoginActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                        Snackbar.make(loginFormView, getString(R.string.user_not_found), Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
                     }
                 });
             }
         });
 
     }
+
+    //Metodo per aprire la schermata adatta dell'app in base al tipo di user
+    private void apriNuovaSchermata(String tipoUser) {
+
+
+
+        if (tipoUser.equals(getString(R.string.user_developer)) || tipoUser.equals(getString(R.string.user_DBA))) {
+
+            finish();
+            activityStart(DBAActivity.class);
+        }
+
+        else if (tipoUser.equals(getString(R.string.user_operaio))) {
+            finish();
+            /*** TEST ***/
+            /*** RIF. 6 ***/
+            //Provo i fragments
+            //activityStart(OperaioActivity.class);
+            activityStart(OperaioActivity2.class);
+        }
+
+        else {
+
+            Log.e(getString(R.string.log_e_DB_read), getString(R.string.user_error));
+            Log.e( getString(R.string.log_e_DB_read), "TIPO USER RILEVATO: " + tipoUser);
+            Snackbar.make(loginFormView, getString(R.string.error_DB_read), Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
+        }
+
+    }
+
+    //Metodo per lanciare correttamente una nuova Activity
+    private void activityStart(Class activity){
+
+        Intent i = new Intent(LoginActivity.this, activity);
+        startActivity(i);
+
+
+    }
+
 }
