f4c0c2c85934bcd43bdd7ada3899cb93afb6ff5c
Andrea <andreacb94>
2017-06-19 23:22:31 +0200

    19.06.08 - Classe Attore commentata

--- app/src/main/java/com/example/andrea/diariostraordinari/Adapter/Attore.java
+++ app/src/main/java/com/example/andrea/diariostraordinari/Adapter/Attore.java
@@ -4,8 +4,14 @@ package com.example.andrea.diariostraordinari.Adapter;
  * Created by Andrea on 25/04/17.
  */
 
+
+/**
+ * Classe Attore per definire la struttura di un oggetto Attore nel DB
+ */
+
 public class Attore {
 
+    //Variabili di classe
     private String idattore;
     private String tipo;
     private String nome;
@@ -13,18 +19,23 @@ public class Attore {
     private String password;
 
 
+    //Metodo ostruttore vuoto richiesto di Default per il cast da parte del DB Firebase
     public Attore(){
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
+    //Metodo costruttore
     public Attore(String idattore, String tipo, String nome, String cognome, String password){
+
         this.idattore = idattore;
         this.tipo = tipo;
         this.nome = nome;
         this.cognome = cognome;
         this.password = password;
+
     }
 
+    //Metodi getter per le proprietà degli oggetti Attore
     public String getIdattore() {
         return idattore;
     }
